591d76e36a89ca635b926cbd6fbb2901001f61f9
Rui Carmo <rui.carmo@gmail.com>
2013-01-27 19:56:08 +0000

    Changed startup steps

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ sys.path.append('lib')
 
 import bottle, utils
 
-# our own modules
-import routes, beacon, playlist, browser
-
+# read configuration file and setup various globals
+config     = utils.get_config(os.path.join(utils.path_for('data'),'config.json'))
+# validate framebuffer settings
+config = utils.validate_resolution(config)
+# setup static file root
+staticroot = utils.path_for('static')
+# check if we have a valid IP address
+ip_address = utils.get_ip_address(config.interface)
 # Shared data used by other modules
 version    = '0.13.01.21.1'
 # Flag for controlled thread termination
 running    = True
 # Screen state sent from server
 screen     = {}
-
-# various
-staticroot = utils.path_for('static')
-config     = utils.get_config(os.path.join(utils.path_for('data'),'config.json'))
-ip_address = utils.get_ip_address(config.interface)
 
 # Defaults sent to templates
 template_vars = {
@@ -38,51 +38,55 @@ template_vars = {
     'debug'     : config.debug
 }
 
+# Set up logging
+logging.config.dictConfig(dict(config.logging))
+log = logging.getLogger()
+
+
 if __name__=='__main__':
-    # Set up logging
-    logging.config.dictConfig(json.loads(str(config.logging)))
-    log = logging.getLogger()
+    if config.debug:
+        if 'BOTTLE_CHILD' not in os.environ:
+            log.debug('Using reloader, spawning first child.')
+        else:
+            log.debug('Child spawned.')
 
-    log.info("Starting application.")
+    if not config.debug or ('BOTTLE_CHILD' in os.environ):
+        log.info("Setting up application.")
+        import routes, playlist, browser, beacon, video
+        log.info("Serving requests.")
 
-    # If we're running on a Pi, try to figure out what the display's set to
-    res = subprocess.Popen('fbset', stdout=subprocess.PIPE, stderr=open(os.devnull,'wb'), shell=True)
-
-    # We're only going to account for one other case here
-    if '"1024x768"' in res.stdout.read().split():
-        config.screen.width = 1024
-        config.screen.height = 768
-    
-    # Check if another instance is still running
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((config.http.bind_address,config.http.port))
-        log.error("Server already running, exiting.")
-        sys.exit(1)
-    except socket.error, msg:
-        pass
-
-    log.info("Socket free.")
-    if ip_address or config.staging:
-        if not config.staging:
-            # Get the browser going
-            u = browser.Browser(config)
-            p = playlist.Player(u, 'playlist.json')
-            log.info("Starting player thread")
-            p.start()
+        # Check if another instance is still running
         try:
-            assert('http' in config.server_url)
-            b = beacon.Beacon(config, utils.get_mac_address(config.interface), ip_address, u)
-            log.info("Starting beacon thread")
-            b.start()
-        except Exception, e:
-            log.info("%s, operating in standalone mode." % e)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((config.http.bind_address,config.http.port))
+            log.error("Server already running, exiting.")
+            sys.exit(1)
+        except socket.error, msg:
             pass
-    else:
-        # Signal for help and stay put. There's no point in debugging the LAN ourselves.
-        b = browser.Browser(config)
-        b.do(config.command['local'] % 'nonet')
-        log.error("Failsafe mode")
+        log.info("Socket free.")
+
+        if ip_address or config.staging:
+            uzbl = None
+            if not config.staging:
+                # Get the browser going
+                uzbl = browser.Browser(config)
+                p = playlist.Player(uzbl, 'playlist.json')
+                log.info("Starting player thread")
+                p.start()
+
+                try:
+                    assert('http' in config.server_url)
+                    b = beacon.Beacon(config, utils.get_mac_address(config.interface), ip_address, uzbl)
+                    log.info("Starting beacon thread")
+                    b.start()
+                except Exception, e:
+                    log.info("%s, operating in standalone mode." % e)
+                    pass
+        else:
+            # Signal for help and stay put. There's no point in debugging the LAN ourselves.
+            uzbl = browser.Browser(config)
+            uzbl.do(config.command['local'] % 'nonet')
+            log.error("Failsafe mode")
 
     bottle.run(
         port     = config.http.port, 
