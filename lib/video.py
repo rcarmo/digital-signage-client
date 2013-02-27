e6abdd02e2cf9dd3c87af7afd61038ab0e1e976a
Rui Carmo <rui.carmo@gmail.com>
2013-02-27 11:45:15 +0000

    Think I finally nailed proper omxplayer termination

--- lib/video.py
+++ lib/video.py
@@ -17,8 +17,7 @@ log = logging.getLogger()
 
 def _handler(player):
     """Timer handler"""
-    log.debug("Terminating player")
-    player.terminate()
+    player.kill()
 
 
 class Player:
@@ -31,24 +30,31 @@ class Player:
 
     def launch(self, uri, timeout=0):
         """Launch omxplayer"""
-
-        # set up timeout
-        if timeout:
-            self.timer = Timer(timeout, _handler, [self])
-            self.timer.start()
-
         # we need to provide at least a valid stdin parameter,
         # otherwise omxplayer will fail.
         # Note that we force audio to "local" to mute HDMI output
         self.omxplayer = Popen(['/usr/bin/omxplayer','-o','local',uri], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        # set up timeout
+        if timeout:
+            self.timer = Timer(timeout, _handler, [self])
+            self.timer.start()
+            log.debug("Started timer for %ds" % timeout)
         self.omxplayer.wait()
 
 
     def terminate(self):
         """Kill the player (softly)"""
-        self.omxplayer.terminate()
+        # Send a quit command to omxplayer -- this _finally_ works properly in the latest versions
+        output = self.omxplayer.communicate('q')[0]
+        log.debug("omxplayer: %s" % output.replace(('\n',' '))
+        # try to quit the process normally in case the usual exit message isn't there.
+        # in my experience omxplayer is notoriously flaky, so this sometimes doesn't work
+        if "nice day" not in output:
+            log.debug("Terminating player")
+            self.omxplayer.terminate()
 
 
     def kill(self):
         """Kill the player"""
+        log.debug("Killing player")
         self.omxplayer.kill()
