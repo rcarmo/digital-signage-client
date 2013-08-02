2940ff136ea4bc04e9e479fb2ab6cd2faa6930cd
Rui Carmo <rui.carmo@gmail.com>
2013-08-02 14:20:18 +0100

    Added new config object

---
+++ lib/config.py
@@ -0,0 +1,14 @@
+import os, sys, platform, logging.config
+from utils import get_config, path_for
+
+try:
+    settings
+except NameError:
+    try:
+        settings = get_config(path_for(os.path.join('..','etc','config.json')))
+    except Exception as e:
+        print >> sys.stderr, ("Error while loading configuration file" % locals())
+        sys.exit(2)
+        logging.config.dictConfig(dict(settings.logging))
+        log = logging.getLogger()
+        log.info("Logging configured."
